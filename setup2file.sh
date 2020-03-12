05840a73ab52b8e6c2407987a977946d789c445f
efesencan <efesencan@s.edu>
2020-03-12 23:14:40 +0300

    Added setupfiles

---
+++ setup2file.sh
@@ -0,0 +1,10 @@
+sudo python3 -m venv env
+sudo source env/bin/activate
+sudo pip3 install -r Requirements.txt
+sudo apt-get install postgresql
+sudo apt-get install postgresql-contrib
+sudo apt-get install libpq-dev
+sudo apt-get install python-dev
+sudo pip3 install psycopg2
+
+
---
+++ setupfile.sh
@@ -0,0 +1,10 @@
+pwd
+sudo apt-get install python3.6
+sudo apt update
+sudo apt install python3-pip
+sudo python3 -m pip install virtualenv
+sudo apt-get install python3-ven
+sudo python3 -m pip install virtualenv
+sudo apt-get install python3-venv
+
+
