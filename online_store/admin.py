cf919c3969a6c3241d8e0f2370e8b36192044093
yigittekinalp <yigittekinalp@sabanciuniv.edu>
2020-03-18 18:33:00 +0300

    update

--- online_store/admin.py
+++ online_store/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['isActive', 'quantity','price','cost','name','modelNo', \
+     'description','warrantyStatus','disturbuterInfo','categoryName','listedDate']
+    
+class BasketAdmin(admin.ModelAdmin):
+    fields = ['cId', 'pId','quantity','totalPrice','purchasedDate','isPurchased']
+
+class DeliveryAdmin(admin.ModelAdmin):
+    fields = ['address', 'IsDelivered']
+
+class ClientAdmin(admin.ModelAdmin):
+    fields = ['name', 'email','address','taxNumber','fId']
+
+class InvoiceAdmin(admin.ModelAdmin):
+    fields = ['cId','bId', 'dId']
+
+class FavouriteAdmin(admin.ModelAdmin):
+    fields = ['cId','pId']
+
+admin.site.register(Product,  ProductAdmin)
+admin.site.register(Basket,    BasketAdmin)
+admin.site.register(Delivery,  DeliveryAdmin)
+admin.site.register(Client,    ClientAdmin)
+admin.site.register(Invoice,   InvoiceAdmin)
+admin.site.register(Favourite, FavouriteAdmin)
--- online_store/models.py
+++ online_store/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 
 
-class Products(models.Model):
+
+class Product(models.Model):
     ### Primary Key
     pId = models.AutoField(primary_key=True) 
     
@@ -13,7 +15,7 @@ class Products(models.Model):
     price            = models.FloatField()
     cost             = models.FloatField()
     name             = models.CharField(max_length=50) # TEXT 
-    modelNO          = models.CharField(max_length=50) # TEXT ,BV200423 universal code
+    modelNo          = models.CharField(max_length=50) # TEXT ,BV200423 universal code
     description      = models.CharField(max_length=500) # TEXT
     warrantyStatus   = models.IntegerField()
     disturbuterInfo  = models.CharField(max_length=100) #TEXT
@@ -30,15 +32,15 @@ class Products(models.Model):
 
 class Basket(models.Model):
     bId            = models.AutoField(primary_key=True)
-    cID            = models.ForeignKey(Client, on_delete=models.SET_NULL)
-    pId            = models.ForeignKey(Products, on_delete=models.SET_NULL) ##### değiştirsek 
+    cId            = models.ForeignKey('Client', null = True,on_delete = models.SET_NULL)
+    pId            = models.ForeignKey('Product', null = True,on_delete = models.SET_NULL) ##### değiştirsek 
     quantity       = models.IntegerField()
     totalPrice     = models.FloatField()
     purchasedDate  = models.DateField()
     isPurchased    = models.NullBooleanField(False)
 
     class Meta:
-        unique_together = (('bId', 'cID'),)
+        unique_together = (('bId', 'cId'),)
 
  #   def Purchase():
   #      isPurchased =True
@@ -59,15 +61,12 @@ class Delivery(models.Model):
     address        = models.CharField(max_length=500)
     IsDelivered    = models.NullBooleanField()
 
-
-
-class Favourites(models.Model):
+class Favourite(models.Model):
     fId            = models.AutoField(primary_key=True)
-    cID            = models.ForeignKey(Client, on_delete=models.SET_NULL)
-    pId            = models.ForeignKey(Products, on_delete=models.SET_NULL) 
+    cId            = models.ForeignKey('Client', null=True,on_delete = models.SET_NULL)
+    pId            = models.ForeignKey('Product', null = True,on_delete = models.SET_NULL) 
     class Meta:
-        unique_together = (('fId', 'cID'),)
-
+        unique_together = (('fId', 'cId'),)
 
 class Client(models.Model):
     cId       = models.AutoField(primary_key=True)
@@ -75,15 +74,13 @@ class Client(models.Model):
     email     = models.EmailField(max_length=254)
     address   = models.CharField(max_length=500)
     taxNumber = models.IntegerField()
-    fId       = models.ForeignKey(Favourites, on_delete=models.SET_NULL)
-    password  = forms.CharField(max_length=32, widget=forms.PasswordInput)
-    
-
+    fId       = models.ForeignKey('Favourite', null = True,on_delete = models.SET_NULL)
+    #password  = forms.CharField(max_length=32, widget=forms.PasswordInput)
 
 
 #add to basket
 #    create new basket item with iscurrent set to true, if no entry with basket is current true.
-#    else 
+#    else x
 #*purchase 
 #basketde isCurrent== True yoksa yeni basket objesi yaratsın
 #ve sadece purchase gerçekleşirse isCurrent = False
@@ -93,9 +90,9 @@ class Invoice(models.Model):
     
     class Meta:
         unique_together = (('iId', 'bId','dId','cId'),)
-    
-    bId = models.ForeignKey(Basket, on_delete=models.SET_NULL) ##### değiştirsek 
-    dId = models.ForeignKey(Delivery, on_delete=models.SET_NULL)
+    cId = models.ForeignKey('Client', null=True,on_delete = models.SET_NULL)
+    bId = models.ForeignKey('Basket', null = True,on_delete = models.SET_NULL) ##### değiştirsek 
+    dId = models.ForeignKey('Delivery', null = True,on_delete = models.SET_NULL)
     
 
 #def  deleteProduct():
